377d7bba11a5744594b94ec04aa0b17f5f7cad0b
michal <michal.swierz1@gmail.com>
2025-02-09 12:33:35 +0100

    Add API endpoints for shortener url

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const isUrl = require('is-url');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// Api endpoints for shortener url
+let url_arr = [];
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  if (!isUrl(url)) return res.json({ "error": "Invalid URL" });
+  if (!url_arr.includes(url)) url_arr.push(url);
+  res.json({ original_url: url, short_url: url_arr.indexOf(url) });
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:text', (req, res) => {
+  res.redirect(url_arr[req.params.text]);
+});
+
+// Listening
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
